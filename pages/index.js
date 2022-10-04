68343ae0369f3e4a9f0596cf1a5c07a7f4465075
Guilherme <guilherme@acheipneus.com.br>
2022-10-04 12:49:46 -0300

    /signin

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,62 @@
-import { Box } from "@chakra-ui/react";
-import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { supabaseClient } from "../lib/client";
+import customTheme from "../lib/theme";
 
-const Home = () => {
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const user = supabaseClient.auth.user();
+
+  useEffect(() => {
+    const { data: authListener } = supabaseClient.auth.onAuthStateChange(
+      (event, session) => {
+        handleAuthSession(event, session);
+        if (event === "SIGNED_IN") {
+          const signedInUser = supabaseClient.auth.user();
+          const userId = signedInUser.id;
+          supabaseClient
+            .from("profiles")
+            .upsert({ id: userId })
+            .then((_data, error) => {
+              if (!error) {
+                router.push("/");
+              }
+            });
+        }
+        if (event === "SIGNED_OUT") {
+          router.push("/signin");
+        }
+      }
+    );
+
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, [router]);
+
+  useEffect(() => {
+    if (user) {
+      if (router.pathname === "/signin") {
+        router.push("/");
+      }
+    }
+  }, [router.pathname, user, router]);
+
+  const handleAuthSession = async (event, session) => {
+    await fetch("/api/auth", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify({ event, session }),
+    });
+  };
+
   return (
-    <div>
-      <Head>
-        <title>TodoApp</title>
-        <meta
-          name="description"
-          content="Awesome todoapp to store your awesome todos"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Box>Hello world</Box>
-      </main>
-    </div>
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
-};
+}
 
-export default Home;
+export default MyApp;
