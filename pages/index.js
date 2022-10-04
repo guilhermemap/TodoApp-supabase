64eab4935fb1cc024b98c4f5269b522f469bbd93
Guilherme <guilherme@acheipneus.com.br>
2022-10-04 13:01:37 -0300

    ManageTodo modal pra adicionar todos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import { useDisclosure } from "@chakra-ui/hooks";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import ManageTodo from "../components/ManageTodo";
 import Navbar from "../components/Navbar";
 import { supabaseClient } from "../lib/client";
 
 const Home = () => {
+  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const router = useRouter();
   const user = supabaseClient.auth.user();
 
@@ -25,7 +30,8 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Navbar />
+        <Navbar onOpen={onOpen} />
+        <ManageTodo isOpen={isOpen} onClose={onClose} initialRef={initialRef} />
       </main>
     </div>
   );
